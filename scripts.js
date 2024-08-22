4d27010463122e0a253d15acb9b87ea40a1aea22
KuromiDev <AliceDevelopments@proton.me>
2024-08-22 17:24:03 +0100

    prettier

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}  
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 function featuresScroll() {
-    document.getElementById("featuresbox").scrollIntoView();
+  document.getElementById("featuresbox").scrollIntoView();
 }
 
 function buyScroll() {
-    document.getElementById("paymentbox").scrollIntoView();
+  document.getElementById("paymentbox").scrollIntoView();
 }
 
 function faqScroll() {
-    document.getElementById("faqbox").scrollIntoView();
+  document.getElementById("faqbox").scrollIntoView();
 }
 
 const EMPTY = "";
@@ -21,21 +21,27 @@ const NAVITEMS = document.getElementById("navitems");
 const NAVITEM_HEIGHT_VARIABLE_NAME = "--navitem-height";
 
 BURGER_MENU.addEventListener("click", () => {
-    let currentNavitemHeight = document.documentElement.style.getPropertyValue(NAVITEM_HEIGHT_VARIABLE_NAME);
+  let currentNavitemHeight = document.documentElement.style.getPropertyValue(
+    NAVITEM_HEIGHT_VARIABLE_NAME
+  );
 
-    if (currentNavitemHeight === EMPTY) {
-        currentNavitemHeight = "0";
-    }
+  if (currentNavitemHeight === EMPTY) {
+    currentNavitemHeight = "0";
+  }
 
-    document.documentElement.style.setProperty(NAVITEM_HEIGHT_VARIABLE_NAME, currentNavitemHeight === "0" ? "8rem" : "0");
+  document.documentElement.style.setProperty(
+    NAVITEM_HEIGHT_VARIABLE_NAME,
+    currentNavitemHeight === "0" ? "8rem" : "0"
+  );
 });
-   
 
-const phrase = ["Unmatched quality, bypasses, and features Supporting Minecraft Java Edition 1.8-1.18."];
+const phrase = [
+  "Unmatched quality, bypasses, and features Supporting Minecraft Java Edition 1.8-1.18.",
+];
 const el = document.getElementById("typewriter");
 
-function sleep(ms){
-    return new Promise((resolve) => setTimeout(resolve, ms));
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 let sleepTime = 20;
@@ -43,23 +49,23 @@ let sleepTime = 20;
 let curPhraseIndex = 0;
 
 const writeLoop = async () => {
-    while (true){
-        let curWord = phrase[curPhraseIndex];
+  while (true) {
+    let curWord = phrase[curPhraseIndex];
 
-        for (let i = 0; i < curWord.length; i++) {
-            el.innerText = curWord.substring(0, i + 1);
-            await sleep(sleepTime);
-        }
-
-        await sleep(sleepTime * 100)
-
-        for (let i = curWord.length; i > 0; i--) {
-            el.innerText = curWord.substring(0, i - 1);
-            await sleep(sleepTime);
-        }
-
-        await sleep(sleepTime * 75)
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
     }
+
+    await sleep(sleepTime * 100);
+
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 75);
+  }
 };
 
 writeLoop();
