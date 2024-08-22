0866a54926f88d907049a431eea9f24188334d29
KuromiDev <AliceDevelopments@proton.me>
2024-08-22 15:29:09 +0100

    bye bye burger

--- scripts.js
+++ scripts.js
@@ -13,37 +13,7 @@ function buyScroll() {
 
 function faqScroll() {
     document.getElementById("faqbox").scrollIntoView();
-}
-
-var clicked = true;
-
-function burgerExpand() {
-    
-    var myContainer = document.getElementById("flex-menubar-id");
-    document.getElementById("signup-button-id").style.pointerEvents = "all";
-    document.getElementById("burger-menu").style.marginTop = "2.5%";
-    if(clicked) {
-      myContainer.style.flexDirection = "column";
-      document.getElementById("burger-menu").style.marginTop = "2.5%";
-      document.getElementById("burger-menu").style.marginLeft = "-8.1%";
-      document.getElementById("flex-menu-item-id").style.opacity = "100";
-      document.getElementById("flex-menu-item-id").style.pointerEvents = "all";
-      document.getElementById("signup-button-id").style.opacity = "100";
-      document.getElementById("signup-button-id").style.pointerEvents = "all";
-      clicked = false;
-    }else {
-      myContainer.style.flexDirection = "row";
-      document.getElementById("burger-menu").style.marginTop = "30%";
-      document.getElementById("burger-menu").style.marginLeft = "-8.1%";
-      document.getElementById("flex-menu-item-id").style.opacity = "0";
-      document.getElementById("flex-menu-item-id").style.pointerEvents = "none";
-      document.getElementById("signup-button-id").style.opacity = "0";
-      document.getElementById("signup-button-id").style.pointerEvents = "none";
-      clicked = true;
-    }
-}
-
-   
+}  
 
 const phrase = ["Unmatched quality, bypasses, and features Supporting Minecraft Java Edition 1.8-1.18."];
 const el = document.getElementById("typewriter");
